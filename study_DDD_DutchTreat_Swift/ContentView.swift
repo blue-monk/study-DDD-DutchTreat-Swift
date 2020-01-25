ee488ca9eb1f56881ebd39a36a6107487a4dcf7b
blue-monk <shino.hosting+github_blue-monk@gmail.com>
2020-01-26 01:48:29 +0900

    initial commit

---
+++ study_DDD_DutchTreat_Swift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  study_DDD_DutchTreat_Swift
+//
+//  Created by blue-monk on 2020/01/26.
+//  Copyright © 2020 blue-monk. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
