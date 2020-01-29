b2f6c13e437c0207ca1bdd29c0dfd976428584d3
blue-monk <shino.hosting+github_blue-monk@gmail.com>
2020-01-29 22:24:33 +0900

    refactor: 差額アジャスターのロジック共通化とメソッドシグネチャ変更
    * 同じような処理を共通化した
    * ランダムも predicate を工夫すればまとめられるとは思う
    * メソッドシグネチャをよりわかりやすくした

--- study_DDD_DutchTreat_Swift/src/domain/model/calculation/adjustment/Adjusters.swift
+++ study_DDD_DutchTreat_Swift/src/domain/model/calculation/adjustment/Adjusters.swift
@@ -8,48 +8,31 @@
 
 import Foundation
 
+/// ［アジャスター］主催者に対して調整
 struct OrganizerSacrificedAdjuster: DifferenceAdjuster {
 
     func adjust(_ difference: DifferenceAmount, in memberPayments: MemberPayments) -> MemberPayments {
-
-        let adjusted = _adjustOne(of: memberPayments, adjustmentAmount: difference, where: \.isPartyOrganizer)
-
-        guard let adjustedMemberPayment = adjusted else {
-            fatalError("パーティメンバーに主催者がいないみたい。")
-        }
-
-        return memberPayments.replacing(memberPayment: adjustedMemberPayment)
+        _adjust(difference, to: .partyOrganizer, in: memberPayments, messageWhenNotExists: "パーティメンバーに主催者がいないみたい...")
     }
 }
 
+/// ［アジャスター］幹事の1人に対して調整
 struct SecretarySacrificedAdjuster: DifferenceAdjuster {
 
     func adjust(_ difference: DifferenceAmount, in memberPayments: MemberPayments) -> MemberPayments {
-
-        let adjusted = _adjustOne(of: memberPayments, adjustmentAmount: difference, where: \.isSecretary)
-
-        guard let adjustedMemberPayment = adjusted else {
-            fatalError("TODO: パーティメンバーに幹事がいなかったら、「調整先に幹事」を選択できないようにする。")
-        }
-
-        return memberPayments.replacing(memberPayment: adjustedMemberPayment)
+        _adjust(difference, to: .secretary, in: memberPayments, messageWhenNotExists: "TODO: パーティメンバーに幹事がいなかったら、「調整先に幹事」を選択できないようにする。")
     }
 }
 
+/// ［アジャスター］一般メンバーの1人に対して調整
 struct GenaralMemberSacrificedAdjuster: DifferenceAdjuster {
 
     func adjust(_ difference: DifferenceAmount, in memberPayments: MemberPayments) -> MemberPayments {
-
-        let adjusted = _adjustOne(of: memberPayments, adjustmentAmount: difference, where: \.isGeneralMember)
-
-        guard let adjustedMemberPayment = adjusted else {
-            fatalError("TODO: パーティメンバーに一般がいなかったら、「調整先に一般」を選択できないようにする。")
-        }
-
-        return memberPayments.replacing(memberPayment: adjustedMemberPayment)
+        _adjust(difference, to: .generalMember, in: memberPayments, messageWhenNotExists: "TODO: パーティメンバーに一般がいなかったら、「調整先に一般」を選択できないようにする。")
     }
 }
 
+/// ［アジャスター］ランダムに選択された1人に対して調整
 struct RandomSacrificedAdjuster: DifferenceAdjuster {
 
     func adjust(_ difference: DifferenceAmount, in memberPayments: MemberPayments) -> MemberPayments {
@@ -59,11 +42,31 @@ struct RandomSacrificedAdjuster: DifferenceAdjuster {
 
 
 
-fileprivate func _adjustOne(of memberPayments: MemberPayments, adjustmentAmount: DifferenceAmount, where predicate: KeyPath<Member, Bool>) -> MemberPayment? {
+fileprivate func _adjust(_ difference: DifferenceAmount, to memberType: MemberType, in memberPayments: MemberPayments, messageWhenNotExists message: String) -> MemberPayments {
+
+    let adjusted = _adjust(difference, in: memberPayments, where: _predicate(for: memberType))
+
+    guard let adjustedMemberPayment = adjusted else {
+        fatalError(message)
+    }
+
+    return memberPayments.replacing(memberPayment: adjustedMemberPayment)
+}
+
+fileprivate func _adjust(_ difference: DifferenceAmount, in memberPayments: MemberPayments, where predicate: KeyPath<Member, Bool>) -> MemberPayment? {
 
     return memberPayments
             .filter() { $0.member[keyPath: predicate] }
             .shuffled()
             .first
-            .map() { $0.addingPaymentAmount(adjustmentAmount) }
+            .map() { $0.addingPaymentAmount(difference) }
+}
+
+fileprivate func _predicate(for memberType: MemberType) -> KeyPath<Member, Bool> {
+
+    switch memberType {
+        case .partyOrganizer: return \.isPartyOrganizer
+        case .secretary:      return \.isSecretary
+        case .generalMember:  return \.isGeneralMember
+    }
 }
